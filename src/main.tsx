5ddbd0c4e415be5dd7cc7fcf202181d3c83cec1c
Firas Dahmani <firas1dahmani@gmail.com>
2024-08-08 14:01:41 +0200

    change

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import HelloTagB from './HelloTagB.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelloTagB />
   </StrictMode>,
 )
